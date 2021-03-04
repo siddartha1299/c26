f4228f47e40f57029b8043f734913a1ac4451c2e
siddartha1299 <suddalasrnivassrnivas@gmail.com>
2021-03-04 19:41:31 +0530

    first

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function setup(){
     log3 =  new Log(810,180,300, PI/2);
 
     box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
+    log4 = new Log(760,120,150, PI/6);
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
